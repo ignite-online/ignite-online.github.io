3c16acbf3f3570301b02181958861af5b2141d2d
harsh18 <harsh.lamba181189@gmail.com>
2016-07-22 03:07:19 +0530

    added constant file

---
+++ olympia/occ-dashboard/app/shared/common/common.constant.js
@@ -0,0 +1,8 @@
+(function(){
+	'use strict'
+
+	angular.module('common.module')
+		.constant('enum', {
+			'URL' : 'http://olympia-jobapplicationprediction-api-test.azurewebsites.net/API/CcoDashboard/GetOccDashBoardReports'
+		});
+})();
